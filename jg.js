9792e5d5cd7d51da6b46b15c7017f6e6f374bdff
kcTOP1 <120762579+kcTOP1@users.noreply.github.com>
2023-05-07 16:13:19 -0400

    Created working addition calculator  using buttons

--- jg.js
+++ jg.js
@@ -1,44 +1,89 @@
-let input1 = prompt("Enter your value: ");
-let input2 = prompt("Enter your value: ");
-let operatorIn = prompt("Enter your operator (1,2,3,4)");
+//let input1 = prompt("Enter your value: ");
+//let input2 = prompt("Enter your value: ");
+//let operatorIn = prompt("Enter your operator (1,2,3,4)");
 
-let numOpp = parseInt(operatorIn);
-let num1 = parseInt(input1);
-let num2 = parseInt(input2);
+//let numOpp = parseInt(operatorIn);
+//let num1 = parseInt(input1);
+//let num2 = parseInt(input2);
 
 
-function add(num1,num2) {
-    alert(num1 + num2);
+//begin button scanning vales
+let num1 = '';
+let sign = '';
+const container = document.querySelector('#container');
+const buttons = document.querySelectorAll('button');
+const oppBtn = document.querySelectorAll('[data-opp]');
+const result = document.querySelectorAll('[data-equal]');
+
+oppBtn.forEach((button) => {
+ button.addEventListener('click', () => setOp(button.textContent))
+})
+
+result.forEach((button) => {
+    button.addEventListener('click', () => evaluate(sign))
+})
+
+
+
+buttons.forEach((button) => {
+        button.addEventListener('click', () => {
+        num1 = num1 + button.textContent;
+        display.textContent = "";
+        display.textContent = num1;
+        actualNum = parseInt(num1);
+        console.log("num1 = " + num1);
+        console.log("Actualnum1 = " + actualNum);        
+     });
+});
+
+
+function setOp(operator) {
+    firstOp = actualNum;
+    sign = operator;
+    currentStep = `${firstOp} ${sign}`
+    console.log('setOp function works: ' + currentStep);
+    num1 = '';
+}
+
+function evaluate() {
+    secondOp = actualNum;
+    if (sign = '+') {
+        add(firstOp,actualNum)
+    }
+    console.log('Equation: ' + `${firstOp} ${sign} ${actualNum}`);
     
 }
 
-function subtract(num1,num2) {
-    alert(num1 - num2);
-}
+    const display = document.createElement('p');
+    display.classList.add('display');
+    container.appendChild(display);
 
-function multiply(num1,num2) {
-    alert(num1 * num2);
-}
+    const final = document.createElement('p');
+    final.classList.add('display');
+    container.appendChild(final);
 
-function divide(num1,num2) {
-    alert(num1 / num2);
-}
 
-function operator() {
-    if (numOpp === 1) {
-        add(num1,num2);
-    } else if (numOpp === 2) {
-        subtract(num1,num2);
-    } else if (numOpp === 3) {
-        divide(num1,num2);
-    } else if (numOpp === 4) {
-        multiply(num1,num2);
-    } else {
-        alert("error");
+    function add(num1,num2) { 
+        //alert(num1 + num2);
+        let sum = num1 + num2;
+        display.textContent = "";
+        final.textContent = sum;
+        console.log('final: ' + sum);
     }
-}
-
-operator(num1,num2);
-
-
-
+    
+    function subtract(num1,num2) {
+        alert(num1 - num2);
+    }
+    
+    function multiply(num1,num2) {
+        alert(num1 * num2);
+    }
+    
+    function divide(num1,num2) {
+        alert(num1 / num2);
+    }
+    
+    function choice() {
+        sign = "on";
+    }
+    
