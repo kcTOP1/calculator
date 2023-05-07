cc1c2c4bc74bae7b58207f29e35657d051a333c4
kcTOP1 <120762579+kcTOP1@users.noreply.github.com>
2023-05-07 17:08:28 -0400

    Added Clear button

--- jg.js
+++ jg.js
@@ -16,13 +16,18 @@ const buttons = document.querySelectorAll('button');
 const oppBtn = document.querySelectorAll('[data-opp]');
 const result = document.querySelectorAll('[data-equal]');
 const numBtn = document.querySelectorAll('[data-number]');
+const clearBtn = document.querySelectorAll('[data-clears]');
 
 oppBtn.forEach((button) => {
- button.addEventListener('click', () => setOp(button.textContent))
+    button.addEventListener('click', () => setOp(button.textContent))
 })
 
 result.forEach((button) => {
     button.addEventListener('click', () => evaluate(sign))
+})
+
+clearBtn.forEach((button) => {
+    button.addEventListener('click', () => clear())
 })
 
 
@@ -37,7 +42,6 @@ numBtn.forEach((button) => {
         console.log("Actualnum1 = " + actualNum);        
      });
 });
-
 
 function setOp(operator) {
     firstOp = actualNum;
@@ -58,6 +62,9 @@ function evaluate() {
     if (sign === '*') {
         multiply(firstOp,actualNum);
     }
+    if (sign === '/') {
+        divide(firstOp,actualNum);
+    }
     console.log('Equation: ' + `${firstOp} ${sign} ${actualNum}`);
     
 }
@@ -75,6 +82,7 @@ function evaluate() {
         let sum = num1 + num2;
         display.textContent = "Equation: " + `${firstOp} ${sign} ${actualNum}`;
         final.textContent = sum;
+        finished = sum;
         console.log('final: ' + sum);
     }
     
@@ -87,12 +95,27 @@ function evaluate() {
     
     function multiply(num1,num2) {
         let mult = num1 * num2;
-        display.textContent = "Equation: " + `${firstOp} ${sign} ${actualNum}`;        final.textContent = mult;
+        display.textContent = "Equation: " + `${firstOp} ${sign} ${actualNum}`;        
+        final.textContent = mult;
         console.log('final: ' + mult);
     }
     
     function divide(num1,num2) {
+        if (actualNum != 0) {
         let divide = num1/num2
-        display.textContent = "Equation: " + `${firstOp} ${sign} ${actualNum}`;        final.textContent = mult;
+        display.textContent = "Equation: " + `${firstOp} ${sign} ${actualNum}`;        
         console.log('final: ' + divide);
+        final.textContent = divide;
+        } else {
+            display.textContent = "Oops, cannot divide by 0";
+        }
+    }
+
+    function clear() {
+        firstOp = 0;
+        num1 = 0;
+        actualNum = 0;
+        display.textContent = "";
+        final.textContent = "";
+        console.log("cleared");
     }
